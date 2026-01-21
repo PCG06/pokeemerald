7b890c8198a52d2d015834db848fc4d4b3462a85
iriv24 <emeraldimperium24@gmail.com>
2026-01-20 22:12:12 -0500

    whitespace test again

--- include/config/debug.h
+++ include/config/debug.h
@@ -1,7 +1,6 @@
 #ifndef GUARD_CONFIG_DEBUG_H
 #define GUARD_CONFIG_DEBUG_H
 
-
 // Overworld Debug
 #define DEBUG_OVERWORLD_MENU            TRUE                // Enables an overworld debug menu to change flags, variables, giving pokemon and more, accessed by holding R and pressing START while in the overworld by default.
 #define DEBUG_OVERWORLD_HELD_KEYS       (A_BUTTON)          // The keys required to be held to open the debug menu.
