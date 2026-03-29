d76588bf51450c542532834fc465f7c54205c262
MaximeGr00 <fgrouazel@gmail.com>
2026-03-29 17:19:25 +0200

    Adding Hall of Origin, Spear Pillar

--- include/constants/region_map_sections.h
+++ include/constants/region_map_sections.h
@@ -220,7 +220,8 @@
 #define MAPSEC_HOT_HOUSE                    0xD8
 #define MAPSEC_TRICK_HOUSE                  0xD9
 #define MAPSEC_DISTORTION_WORLD             0xDA
-#define MAPSEC_NONE                         0xDB
+#define MAPSEC_SPEAR_PILLAR                 0xDB
+#define MAPSEC_NONE                         0xDC
 
 #define METLOC_SPECIAL_EGG                  0xFD
 #define METLOC_IN_GAME_TRADE                0xFE
