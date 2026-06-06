98d8953d84678ba7ab7ac8e84e2f536aec695e95
PCG06 <pcggaming06@gmail.com>
2026-06-06 22:46:02 +0530

    Revert rogue battle speed up

--- src/palette.c
+++ src/palette.c
@@ -33,7 +33,6 @@ ALIGNED(4) EWRAM_DATA u16 gPlttBufferUnfaded[PLTT_BUFFER_SIZE] = {0};
 ALIGNED(4) EWRAM_DATA u16 gPlttBufferFaded[PLTT_BUFFER_SIZE] = {0};
 EWRAM_DATA struct PaletteFadeControl gPaletteFade = {0};
 static EWRAM_DATA u32 sPlttBufferTransferPending = 0;
-static EWRAM_DATA u32 sPlttPreviousUpdateResult = 0; // Fast Battle Speed
 
 static const u8 sRoundedDownGrayscaleMap[] = {
      0,  0,  0,  0,  0,
@@ -84,8 +83,6 @@ u32 UpdatePaletteFade(void)
 {
     u32 result;
 
-    sPlttPreviousUpdateResult = PALETTE_FADE_STATUS_LOADING;
-
     if (sPlttBufferTransferPending)
         return PALETTE_FADE_STATUS_LOADING;
 
@@ -99,14 +96,8 @@ u32 UpdatePaletteFade(void)
         result = UpdateHardwarePaletteFade();
 
     sPlttBufferTransferPending = gPaletteFade.multipurpose1;
-    sPlttPreviousUpdateResult = result;
 
     return result;
-}
-
-u32 PrevPaletteFadeResult(void)
-{
-    return sPlttPreviousUpdateResult;
 }
 
 void ResetPaletteFade(void)
